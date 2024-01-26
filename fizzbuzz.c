b74637609917af9fb45feb79d3adc13e1be90110
molliem16 <molliem16@vt.edu>
2024-01-26 13:06:54 -0500

    :wq
    Added fizzbuzz and languages

---
+++ fizzbuzz.c
@@ -0,0 +1,17 @@
+for(int i = 0; i < 20; i++)
+{
+    if ( i % 3 == 0 && i % 5 == 0)
+    {
+        System.out.println("FizzBuzz");
+    } else {
+        if ( i % 3 == 0)
+        {
+            System.out.println("Fizz");
+        } else {
+            if ( i % 5 == 0)
+            {
+                System.out,println("Buzz");
+            }
+        }
+    }
+}
